7c2d511c5dfd3ca498b4d6d68050f3c2da1f620c
kazukimizu <zm18268@josai.ac.jp>
2020-10-20 02:55:57 +0000

    1020

--- ex6-5.js
+++ ex6-5.js
@@ -5,5 +5,5 @@
 //function taro() {who = '太郎';}
 //function hanako() {who = '花子';}
 var who = 'world';function sayhello65() {alert('Hello, ' + who + '!');}
-var who = '太郎';function taro65()  {alert( who+'が来た');}
-var who = '花子';function hanako65()  {alert( who+'が来た');}
+var who = '太郎';function taro65()  {alert('太郎が来た');}
+var who = '花子';function hanako65()  {alert('花子が来た');}
