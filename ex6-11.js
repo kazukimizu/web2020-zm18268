c7880564b377bafb17723318d515daa0767835d2
kazukimizu <zm18268@josai.ac.jp>
2020-12-07 23:41:04 +0000

    1208

--- ex6-11.js
+++ ex6-11.js
@@ -1,4 +1,4 @@
-function ChangeImageAndBack(){imgback();ThreeSecChange2();}
-function ThreeSecChange2(){setTimeout("imgchange2()",3000);}
-function imgchange2() {document.getElementById('img13').src = './image/JosaiLogo.png';}
-function imgback() {document.getElementById('img13').src = './image/silver_car.png';}
+function ChangeImageAndBack(){imgback611();ThreeSecChange611();}
+function ThreeSecChange611(){setTimeout("imgchange611()",3000);}
+function imgchange611() {document.getElementById('img13').src = './image/JosaiLogo.png';}
+function imgback611() {document.getElementById('img13').src = './image/silver_car.png';}
