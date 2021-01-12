01424c20d617f78701183ebd091dd30c1eb55bfa
kazukimizu <zm18268@josai.ac.jp>
2021-01-12 03:07:18 +0000

    0112

--- ex6-11.js
+++ ex6-11.js
@@ -1,13 +1,13 @@
-function hyoji1() { 
+function hyoji611() { 
 //表示する文字
 var str = "一文字ずつ表示します。"; 
 //テキストボックスの文字数
-var cnt = document.timer1.moji1.value.length;
+var cnt = document.timer611.moji611.value.length;
 //文字が全部表示されているか確認
 if ( cnt < 11 ) { 
 //現在より1文字多く切り出して表示
-document.timer1.moji1.value = str.substr(0,cnt+1); } else {
+document.timer611.moji611.value = str.substr(0,cnt+1); } else {
 //全て表示されたら、空文字に戻す
-document.timer1.moji1.value = ""; } 
+document.timer611.moji611.value = ""; } 
 //setTimeout()を含む関数を呼び出す
-setTimeout("hyoji1()",1000); }
+setTimeout("hyoji611()",1000); }
