0b99b5fe0b75275de24067d8cb7f0571134e1ca0
kazukimizu <zm18268@josai.ac.jp>
2021-01-12 02:33:53 +0000

    0112

---
+++ ex6-12.js
@@ -0,0 +1,4 @@
+function ChangeImageAndBack(){imgback612();ThreeSecChange612();}
+function ThreeSecChange612(){setTimeout("imgchange612()",3000);}
+function imgchange612() {document.getElementById('img13').src = './image/JosaiLogo.png';}
+function imgback612() {document.getElementById('img13').src = './image/silver_car.png';}
